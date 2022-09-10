55bf5a8e13cc71d2f6ecf3548d2de76bb1e0f11c
Alvaro Morales <amorales@integralit.cl>
2022-09-10 15:57:31 -0300

    Feature: Product API REST (searchProduct)

--- controllers/products.js
+++ controllers/products.js
@@ -21,3 +21,16 @@ module.exports.getProductById = catchAsync(async (req, res) => {
     res.send(result);
   });
 });
+
+module.exports.searchProduct = catchAsync(async (req, res) => {
+  const searchValue = req.body.searchValue.trim(); //we define our search value and trim it just in case it comes with whitespaces
+  const categoryId = req.body.categoryId; //we define our categoryId for filtering
+
+  const sql = "SELECT * FROM product WHERE category=? AND name REGEXP ?  "; //we select everything from the product table where the name passed by the user will try to match the entries
+
+  //we pass the string defined in the constant and the productId provided in the url (to return only the instance that matches the id) as a query and return the response as a JSON Object
+  conn.query(sql, [categoryId, searchValue], (err, result) => {
+    if (err || !result) res.status(500).send({ success: false });
+    res.send(result);
+  });
+});
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,14 @@
 //imports
 const express = require("express");
 const router = express.Router();
-const { getProducts, getProductById } = require("./../controllers/products"); //we import the controllers for use in its corresponding route
+const {
+  getProducts,
+  getProductById,
+  searchProduct,
+} = require("./../controllers/products"); //we import the controllers for use in its corresponding route
 
 router.get("/", getProducts);
 router.get("/:id", getProductById);
+router.post("/search", searchProduct);
 
 module.exports = router; //we export the router to be used in the entry point of our app (app.js)
